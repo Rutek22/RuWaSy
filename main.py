7092134e59001786bd9bc856280d5a4bd293122f
Rutek22 <krystian.rutkowski17@gmail.com>
2023-10-23 15:30:27 +0200

    generowanie histogramu etap3

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import matplotlib.pyplot as plt
 
+def generowanie_histogramu(text):
+
+    text_zmieniony = ''.join(filter(str.isalpha, text)).lower()
+
+    litery = []
+    ilosc = []
+
+    for char in text_zmieniony:
+        if char not in litery:
+            litery.append(char)
+            ilosc.append(1)
+        else:
+            index = litery.index(char)
+            ilosc[index] += 1
+
+    plt.bar(litery, ilosc)
+    plt.xlabel("Litry")
+    plt.ylabel("Ilosc")
+    plt.title("Histogram częstości liter")
+
+    plt.savefig('histogram.png')
+
+
 with open('tekst.txt', 'r') as file:
     tekst = file.read()
+
+generowanie_histogramu(tekst)
