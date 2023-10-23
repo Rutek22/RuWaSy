74d398bc9bfccda2541a97d8f81669e9cf678275
Rutek22 <143396339+Rutek22@users.noreply.github.com>
2023-10-23 16:44:49 +0200

    Utworzenie menu oraz dodanie możliwości generowania histogramu na podstawie tekstu z klawiatury (#2)
    * Utworzenie menu
    
    * Sprawdzenie poprawnej opcji
    
    * Obsluga wybranej opcji
    
    * Poprawienie poprawnosci sprawdzania wybranej opcji

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def generowanie_histogramu(text):
 def generowanie_histogramu_z_ograniczonym_zakresem(text,zakres):
 
     text_zmieniony =list(text)
-
+    
     scope = list(zakres)
     litery=[]
     ilosc = []
@@ -42,10 +42,26 @@ def generowanie_histogramu_z_ograniczonym_zakresem(text,zakres):
 
     plt.savefig('histogram_z_zakresem.png')
 
+print("Menu: ")
+print("1 - Odczyt tekstu z pliku tekstowego")
+print("2 - Odczyt tekstu wpisanego z klawiatury")
+opcja = int(input("Wybierz opcje z menu: "))
 
+while (opcja != 1) | (opcja != 2):
+    print("Wybrano nieprawidłową opcje !!!")
+    opcja = int(input("Wybierz opcje z menu: "))
 
-with open('tekst.txt', 'r') as file:
-   tekst = file.read()
-wpisany_tekst=input("Wpisz tekst: ")
-scope=input("Wpisz litery których czestotliwość ma być przedstawiona na histogramie: ")
-generowanie_histogramu_z_ograniczonym_zakresem(wpisany_tekst,scope)
+if (opcja == 1):
+    scope=input("Wpisz litery których czestotliwość ma być przedstawiona na histogramie: ")
+    with open('tekst.txt', 'r') as file:
+        tekst = file.read()
+
+    generowanie_histogramu_z_ograniczonym_zakresem(wpisany_tekst,scope)
+
+else:
+    print("Wprowadź tekst z klawiatury: ")
+    tekst = input("Twój tekst: ")
+    scope=input("Wpisz litery których czestotliwość ma być przedstawiona na histogramie: ")
+    generowanie_histogramu_z_ograniczonym_zakresem(wpisany_tekst,scope)
+
+print("Operacja została wykonana. Histogram został utworzony")
